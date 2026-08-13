cafa18d44e4185e454eeaae35fbeaf1715cc686c
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-14 06:43:42 +0800

    feat: page sign in

---
+++ src/pages/auth/SignIn.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import Logo from '../../components/header/Logo.jsx'
+import { Link } from 'react-router'
+import { LuEye, LuEyeClosed } from 'react-icons/lu'
+import { FaGithub, FaGoogle } from 'react-icons/fa'
+
+function SignIn() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div className="w-8/10 md:w-6/10">
+        <div className='flex md:hidden'>
+          <Logo/>
+        </div>
+
+        <div className="pt-32 w-full">
+          <span className='text-2xl font-bold text-dark-primary'>Welcome back</span>
+          <div className="pt-4">
+            <span className='f-14 text-font-forthy'>Don't have an account? </span>
+            <Link className='f-14 text-primary font-medium' to="/auth/signup">Sign up</Link>
+          </div>
+        </div>
+
+        <div className="flex pt-28 justify-between gap-12">
+          <div className='my-center gap-8 f-14 text-font-fivethy px-16 py-10 border rounded-lg border-border-header w-full'>
+            <FaGoogle/>
+            <span>Google</span>
+          </div>
+          <div className='my-center gap-8 f-14 text-font-fivethy px-16 py-10 border rounded-lg border-border-header w-full'>
+            <FaGithub/>
+            <span>GitHub</span>
+          </div>
+        </div>
+
+        <div className='grid grid-cols-[1fr_auto_1fr] items-center gap-12 pt-20'>
+          <div className='w-full border-t h-0 border-border-header'></div>
+          <span className='text-xs text-font-secondary whitespace-nowrap'>or continue with email</span>
+          <div className="w-full border-t h-0 border-border-header"></div>
+        </div>
+
+        <form className='pt-20'>
+          <div className='flex flex-col gap-6'>
+            <label 
+              className='f-14 text-font-fivethy' 
+              htmlFor="email"
+            >Email address</label>
+            <input 
+              className='bg-light border border-border-header py-10 px-12 f-14 text-font-secondary outline-none rounded-lg'
+              type="text" 
+              name="email" 
+              id="email" 
+              placeholder="alex@example.com" 
+            />
+            <span className='text-font-error text-xs'>Email is required</span>
+          </div>
+
+          <div className='flex flex-col gap-6 py-12'>
+            <div className="flex justify-between items-center">
+              <label
+                className='f-14 text-font-fivethy'
+                htmlFor="password"
+              >Password</label>
+              <Link 
+                className='text-xs text-primary' 
+                to="/auth/password"
+              >Forgot password?</Link>
+            </div>
+            <div className="flex border border-border-header items-center justify-between rounded-lg overflow-hidden px-12">
+              <input
+                className='bg-light py-10 f-14 text-font-secondary w-full outline-none'
+                type="password"
+                name="password"
+                id="password"
+                placeholder="********"
+              />
+              {true ? (<LuEye/>) : (<LuEyeClosed/>)}
+            </div>
+            <span className='text-font-error text-xs'>Password is required</span>
+          </div>
+          <button
+            className='bg-primary py-12 w-full my-center rounded-lg f-14 text-light font-semibold'
+          >Sign in</button>
+        </form>
+
+        <div className='my-center gap-5 pt-16'>
+          <span className='text-xs text-font-secondary'>Just browsing?</span>
+          <Link
+            className='text-xs text-font-secondary underline'
+            to="/"
+          >Continue as guest →</Link>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
