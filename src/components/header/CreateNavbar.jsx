a6630af555f459a9707b7f396a7aa3602622ec78
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-13 16:15:34 +0800

    feat: commponen create navbar

---
+++ src/components/header/CreateNavbar.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router"
+
+function CreateNavbar ({user}) {
+  
+    const listNavbar = [
+      { navbar: "Explore", link: "/explore" },
+      { navbar: "Events", link: "/events" },
+      { navbar: "Communities", link: "/communities" },
+      { navbar: "My Events", link: "/myevents" },
+    ];
+    
+    const listNavbarGuest = [
+      { navbar: "Events", link: "/events" },
+      { navbar: "Communities", link: "/communities" },
+    ];
+    
+    const currentNavbar = 
+    user.role === "admin" || user.role === "attendee" || user.role === "organizer"
+    ? listNavbar
+    : listNavbarGuest;
+    
+    return (<nav>
+      <ul className="flex w-full">
+        {currentNavbar.map((n, i) => {
+          return (
+            <li 
+            className="py-6 px-12 text-font-primary font-medium my-center"
+              key={i}
+            >
+              <Link 
+                className="f-14"
+                to={n.link}
+                >{n.navbar}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+export default CreateNavbar
