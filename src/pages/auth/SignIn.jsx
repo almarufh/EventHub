1e0ab8317482bdffc06b3c80c6988e19d1397277
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-16 21:39:57 +0800

    feat: validasi signIn

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../components/header/Logo.jsx'
-import { Link } from 'react-router'
+import { Link, useNavigate} from 'react-router'
 import { LuEye, LuEyeClosed } from 'react-icons/lu'
 import { FaGithub, FaGoogle } from 'react-icons/fa'
+import { useForm } from 'react-hook-form'
+import { checkEmailUser, userLogin } from '../../utils/user.js'
 
 function SignIn() {
+  const {register, handleSubmit, formState: { errors }} = useForm()
+  const [show, setShow] = useState({value: false, type: "password"})
+  const [getUser, setUser] = useState({email: "", password: ""})
+  const navigate = useNavigate()
+
+  const passwordShow = () => {
+    setShow((prevState) => ({
+      ...prevState,
+      ...{
+        value: !prevState.value,
+        type: prevState.value ? "password" : "text"
+      }
+    }));
+  };
+
+  function sign(){
+    const {status} = userLogin(getUser)
+    if(status) {
+      navigate("/explore")
+    }
+  }
+
   return (
     <div className='w-full h-screen flex justify-center items-center'>
       <div className="w-8/10 md:w-6/10">
@@ -37,7 +61,10 @@ function SignIn() {
           <div className="w-full border-t h-0 border-border-header"></div>
         </div>
 
-        <form className='pt-20'>
+        <form 
+          className='pt-20'
+          onSubmit={handleSubmit(sign)}
+        >
           <div className='flex flex-col gap-6'>
             <label 
               className='f-14 text-font-fivethy' 
@@ -45,12 +72,25 @@ function SignIn() {
             >Email address</label>
             <input 
               className='bg-light border border-border-header py-10 px-12 f-14 text-font-secondary outline-none rounded-lg'
-              type="text" 
-              name="email" 
+              type="email" 
+              {...register("email", { 
+                required: "Email is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Format email tidak valid"
+                },
+                validate: (value)=>{
+                  const {status, user, message} = checkEmailUser(value)
+                  if(user !== undefined){
+                    setUser(user)
+                  }
+                  return status || "Email belum terdaftar"
+                }
+              })} 
               id="email" 
               placeholder="alex@example.com" 
             />
-            <span className='text-font-error text-xs'>Email is required</span>
+            <span className={`${errors?.email ? "opacity-100" : "opacity-0"} text-font-error text-xs`}>{errors.email?.message || "error"}</span>
           </div>
 
           <div className='flex flex-col gap-6 py-12'>
@@ -67,14 +107,26 @@ function SignIn() {
             <div className="flex border border-border-header items-center justify-between rounded-lg overflow-hidden px-12">
               <input
                 className='bg-light py-10 f-14 text-font-secondary w-full outline-none'
-                type="password"
-                name="password"
+                type={show.type}
+                {...register("password", {
+                  required: "Password is required", minLength:{
+                  value: 8,
+                  message: "Password minimal 8 karakter"
+                },
+                validate: (value)=>{
+                  return btoa(getUser.password) === value || "Wrong Password"
+                }
+              }
+              )}
                 id="password"
                 placeholder="********"
               />
-              {true ? (<LuEye/>) : (<LuEyeClosed/>)}
+              <div 
+                onClick={passwordShow}
+                className='cursor-pointer'
+              >{show.value ? (<LuEye/>) : (<LuEyeClosed/>)}</div>
             </div>
-            <span className='text-font-error text-xs'>Password is required</span>
+            <span className={`${errors?.password ? "opacity-100" : "opacity-0"} text-font-error text-xs`}>{errors.password?.message || "error"}</span>
           </div>
           <button
             className='bg-primary py-12 w-full my-center rounded-lg f-14 text-light font-semibold'
