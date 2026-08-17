56c5bfee779e924c8ca1add42d428e66c907710e
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-17 17:04:45 +0800

    feat: assets image

---
+++ src/assets/events/index.js
@@ -0,0 +1,28 @@
+import goConcurrencyWorkshop from './Image (Go Concurrency Workshop).svg';
+import aiProductDesignSummit from './Image (AI Product Design Summit).svg';
+import uxResearchDeepDive from './Image (UX Research Deep Dive).svg';
+
+
+import bandungGoCommunity from './Image (Bandung Go Community).svg';
+import frontendCraftConference from './Image (Frontend Craft Conference).svg';
+import indonesiaFrontendDevs from './Image (Indonesia Frontend Devs).svg';
+import jakartaAIMLClub from './Image (Jakarta AI & ML Club).svg';
+import musicProductionBootcamp from './Image (Music Production Bootcamp).svg';
+import productManagementMasterclass from './Image (Product Management Masterclass).svg';
+import productMindsIndonesia from './Image (Product Minds Indonesia).svg';
+import startupPitchNight from './Image (Startup Pitch Night).svg';
+
+export {
+  bandungGoCommunity,
+  frontendCraftConference,
+  indonesiaFrontendDevs,
+  jakartaAIMLClub,
+  musicProductionBootcamp,
+  productManagementMasterclass,
+  productMindsIndonesia,
+  startupPitchNight,
+  
+  goConcurrencyWorkshop,
+  aiProductDesignSummit,
+  uxResearchDeepDive
+};
