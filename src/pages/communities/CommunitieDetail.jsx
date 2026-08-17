4d437b102f4baf47a96934fcadd353d692c5c2ab
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-17 17:24:19 +0800

    feat: detail communities

---
+++ src/pages/communities/CommunitieDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+function CommunitieDetail() {
+  return (
+    <>
+    <div>CommunitieDetai</div>
+    <Outlet/>
+    </>
+  )
+}
+
+export default CommunitieDetail
