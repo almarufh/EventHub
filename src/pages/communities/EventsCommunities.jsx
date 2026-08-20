b10d5a87f20e475dc486726ba4a8c5a7e599e6a9
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-21 06:41:56 +0800

    fix: layout use max w 7xl

--- src/pages/communities/EventsCommunities.jsx
+++ src/pages/communities/EventsCommunities.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { useParams } from "react-router"
+import CardEvents from "../../components/events/CardEvents.jsx"
 
 function EventsCommunities() {
+  const {id} = useParams()
   return (
-    <div>EventsCOmmunities</div>
+    <section className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-7xl mt-10">
+      <CardEvents community={id} />
+    </section>
   )
 }
 
