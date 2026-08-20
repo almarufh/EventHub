4bb4f2244cf0b1d8b3f81df11d4494f62c91325e
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-21 06:42:17 +0800

    fix: layout use max w 7xl

--- src/pages/communities/MembersCommunities.jsx
+++ src/pages/communities/MembersCommunities.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useParams } from 'react-router'
+import { getMemberCommunity } from '../../database/data'
+import CreateUser from '../../components/CreateUser.jsx'
 
 function MembersCommunities() {
+  const {id} = useParams()
+  const members = getMemberCommunity(id)
   return (
-    <div>MembersCommunities</div>
+    <section className='mt-10 h-screen w-full max-w-7xl'>
+      <div className="grid grid-cols-1 md:grid-cols-3 w-full gap-8 ">
+        <CreateUser data={members} />
+      </div>
+    </section>
   )
 }
 
