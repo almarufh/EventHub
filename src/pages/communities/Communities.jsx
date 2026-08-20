bb2f144988d545f9ce3ccacc6aa9a7aeca89da50
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-21 06:41:22 +0800

    fix: layout use max w 7xl

--- src/pages/communities/Communities.jsx
+++ src/pages/communities/Communities.jsx
@@ -2,7 +2,7 @@ import CardCommunities from '../../components/communities/CardCommunities.jsx'
 
 function Communities() {
   return (
-    <section className='grid grid-cols-4 px-16 gap-8'>
+    <section className='grid grid-cols-1 md:grid-cols-4 px-16 gap-8'>
       <CardCommunities />
     </section>
   )
