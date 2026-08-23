557b842a67785c0228b76a0b84e66af847c6dbf9
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-23 17:01:03 +0800

    feat: reducer and actions for create redux

---
+++ src/redux/slice/user.js
@@ -0,0 +1,113 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState  = {
+    user: {
+        users: [
+            {
+                id: "1787470403207",
+                email: "hidayatmaruf99@gmail.com",
+                name: "alma'ruf hidayat",
+                password: "12345678",
+                role: "attendee"
+            }
+        ],
+        isLoading: false,
+        isSuccess: false,
+        isError: false,
+        message: null
+    },
+    actived: {
+        id: "123456",
+        isActive: false,
+        role: null,
+        isError: false,
+        message: null
+    },
+    userExist: {
+        isExist: false,
+        message: null,
+        data: {}
+    },
+    isPending: false,
+    isSuccess: false,
+    isError: false,
+    message: null,
+}
+
+const eventHub = createSlice({
+    name: "eventHub",
+    initialState,
+    reducers: {
+
+        registerUser: (state, {payload}) => {
+            state.user.isLoading = true
+            state.user.isError = false
+            state.user.isSuccess = false
+            state.user.message = null
+            const isReady =  state.user.users.some((e)=> e.email === payload.email)
+
+            if(isReady) {
+                state.user.isLoading = false
+                state.user.isError = true
+                state.user.message = "Email sudah terdaftar !"
+                return
+            }
+
+            state.user.isLoading = false
+            state.user.isSuccess = true
+            state.user.message = `${payload.email} berhasil terdaftar !`
+            state.user.users.push(payload)
+        },
+        checkEmail: (state, {payload}) => {
+            state.userExist.isExist = false
+            state.userExist.message = null
+            const isReady =  state.user.users.find((e)=> e.email === payload)
+
+            if (!isReady) {
+                state.userExist.message = "email belum terdaftar !"
+                return
+            }
+            
+            state.userExist.isExist = true
+            state.userExist.message = `${payload} ready exist !`
+            state.userExist.data = isReady
+        },
+        login: (state, {payload})=> {
+            state.actived.isError = false
+            state.actived.isActive = false
+            state.actived.id = null
+            state.actived.message = null
+            state.actived.role = null
+            const isReady =  state.user.users.find((e)=> e.email === payload.email)
+
+            if (!isReady) {
+                state.actived.isActive = false
+                state.actived.isError = true
+                state.actived.message = "email salah atau belum terdaftar"
+                return
+            }
+
+            if (btoa(isReady.password) !== payload.password) {
+            // if (isReady.password !== payload.password) {
+                state.actived.isActive = false
+                state.actived.isError = true
+                state.actived.message = "Pasword Salah !"
+                return
+            }
+            
+            state.actived.isError = false
+            state.actived.isActive = true
+            state.actived.id = isReady.id
+            state.actived.message = null
+            state.actived.role = isReady.role
+        }
+
+    }
+})
+
+export const {
+    registerUser, 
+    login,
+    checkEmail,
+} = eventHub.actions
+export default eventHub.reducer
