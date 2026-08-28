b94a790d163fb079eb6cb7506e7b3a012a68bd90
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-28 21:47:04 +0700

    feat: add modal for confirm login

---
+++ src/modals/ConfirmLogout.jsx
@@ -0,0 +1,44 @@
+import { IoClose } from 'react-icons/io5'
+import { LuLogOut } from 'react-icons/lu'
+import { useNavigate } from 'react-router'
+import { useEventHub } from '../hooks/useEventHub'
+
+function ConfirmLogout({modal}) {
+  const {dispatch, handleLogout} = useEventHub()
+  const navigate = useNavigate()
+  return (
+    <main className="inset-0 z-60 fixed h-screen w-full bg-black/10 px-36 flex items-center justify-center">
+        <div className="flex w-full max-w-md bg-light mx-auto p-12 flex-col border border-border-header rounded-xl">
+          <div className="flex justify-between items-center">
+            <span className='f-16 py-32 text-dark font-bold'>Confirm Log Out ?</span>
+            <div className="hover:text-font-error cursor-pointer"
+                onClick={()=> {
+                    navigate("/auth/signin")
+            }}>
+              <IoClose
+                className=''/>
+            </div>
+          </div>
+          <div className="bg-primary8 p-24 flex w-fit round-8 mx-auto my-32 text-primary">
+              <LuLogOut />
+          </div>
+          <div className="flex gap-8 items-center justify-between mb-12">
+            <span
+                onClick={(e)=> {
+                  e.preventDefault()
+                  modal.setConfirmLogout(false)
+                  navigate("/auth/signin")
+                  dispatch(handleLogout())
+            }} className='px-16 f-14 text-dark font-medium py-8 round-8 bg-border-header'>Confirm</span>
+            <span 
+              onClick={()=> {
+                modal.setConfirmLogout(false)
+              }}
+              className='px-16 f-14 text-light font-medium py-8 round-8 bg-primary'>Keep Browsing</span>
+          </div>
+        </div>
+    </main>
+  )
+}
+
+export default ConfirmLogout
