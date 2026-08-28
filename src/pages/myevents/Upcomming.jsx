8eeba00acdf135af540cbb6dbec70e136eb61258
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-29 04:10:12 +0700

    feat: page upcomming in myEvents

--- src/pages/myevents/Upcomming.jsx
+++ src/pages/myevents/Upcomming.jsx
@@ -1,6 +1,8 @@
 function Upcomming() {
   return (
-    <div>Upcomming</div>
+        <main className="h-screen w-full bg-black flex items-center justify-center">
+            <span className="font-bold text-2xl text-light">Upcomming in maintanance</span>
+        </main>
   )
 }
 
