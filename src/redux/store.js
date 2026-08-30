0c5ebb9ef3d744a91abed10b4b62f0af52c988de
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-30 13:07:14 +0700

    feat: add slice auth, events, communities, category, users

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,48 +1,92 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { 
-    persistStore, 
-    FLUSH, 
-    REHYDRATE, 
-    PAUSE, 
-    PERSIST, 
-    PURGE, 
+import { configureStore } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
     REGISTER,
-    persistReducer
-} from 'redux-persist'
+} from "redux-persist";
 import storage from "redux-persist/es/storage";
-import reducer from './slice/user.js'
 
-const eventPersitConfig = {
-    key: "event",
-    storage
-}
+import eventsReducer from "./slice/events.js";
+import categoryReducer from "./slice/category.js";
+import discussionsReducer from "./slice/discussions.js";
+import communitiesReducer from "./slice/communities.js";
+import usersReducer from "./slice/users.js";
+import userReducer from "./slice/user.js";
+import authReducer from "./slice/auth.js";
 
+const eventsPersistConfig = { 
+    key: "events", 
+    storage,
+    whitelist: ["data"]
+};
 
-const eventReducer = combineReducers ({
-    eventHub: reducer
-})
+const categoryPersistConfig = { 
+    key: "category", 
+    storage,
+    whitelist: ["data"]
+};
 
-const events = persistReducer(eventPersitConfig, eventReducer);
+const discussionsPersistConfig = { 
+    key: "discussions", 
+    storage,
+    whitelist: ["data"]
+};
 
+const communitiesPersistConfig = { 
+    key: "communities", 
+    storage,
+    whitelist: ["data"]
+};
+
+const usersPersistConfig = { 
+    key: "users", 
+    storage,
+    whitelist: ["data"]
+};
+
+const userPersistConfig = { 
+    key: "user", 
+    storage,
+    whitelist: ["data"]
+};
+
+const authPersistConfig = { 
+    key: "auth", 
+    storage,
+    whitelist: ["data"]
+};
 
 const store = configureStore({
-    reducer: events,
+    reducer: {
+        events: persistReducer(eventsPersistConfig, eventsReducer),
+        category: persistReducer(categoryPersistConfig, categoryReducer),
+        discussions: persistReducer(discussionsPersistConfig, discussionsReducer),
+        communities: persistReducer(communitiesPersistConfig, communitiesReducer),
+        users: persistReducer(usersPersistConfig, usersReducer),
+        eventHub: persistReducer(userPersistConfig, userReducer),
+        auth: persistReducer(authPersistConfig, authReducer),
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH, 
+                    REHYDRATE, 
+                    PAUSE, 
+                    PERSIST, 
+                    PURGE, 
+                    REGISTER
+                ],
+            },
+        }),
     devTools: true,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions:[
-                FLUSH, 
-                REHYDRATE, 
-                PAUSE, 
-                PERSIST, 
-                PURGE, 
-                REGISTER
-            ]
-        }
-    })
 });
 
+export const persistor = persistStore(store);
 
-export const persistor = persistStore(store)
-
-export default store
+export default store;
