bb2c8b081e72440266fc20e729fdf73d1fa9e729
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-31 07:00:03 +0700

    feat: add data reference create badge

--- src/components/dashboard/CreateBadge.jsx
+++ src/components/dashboard/CreateBadge.jsx
@@ -1,4 +1,4 @@
-function CreateBadge({status}) {
+    function CreateBadge({status}) {
     const create = [
         {
             status: "Active",
@@ -39,13 +39,88 @@ function CreateBadge({status}) {
             status: "organizer",
             color: "text-orange-600",
             bg: "bg-orange-600/10"
+        },
+        {
+            status: "technology",
+            color: "text-blue-500",
+            bg: "bg-blue-500/10"
+        },
+        {
+            status: "artificial intelligence",
+            color: "text-purple-500",
+            bg: "bg-purple-500/10"
+        },
+        {
+            status: "backend engineering",
+            color: "text-indigo-500",
+            bg: "bg-indigo-500/10"
+        },
+        {
+            status: "frontend development",
+            color: "text-sky-500",
+            bg: "bg-sky-500/10"
+        },
+        {
+            status: "mobile development",
+            color: "text-teal-500",
+            bg: "bg-teal-500/10"
+        },
+        {
+            status: "cloud & devops",
+            color: "text-cyan-500",
+            bg: "bg-cyan-500/10"
+        },
+        {
+            status: "cybersecurity",
+            color: "text-rose-500",
+            bg: "bg-rose-500/10"
+        },
+        {
+            status: "data science",
+            color: "text-emerald-500",
+            bg: "bg-emerald-500/10"
+        },
+        {
+            status: "ui/ux design",
+            color: "text-pink-500",
+            bg: "bg-pink-500/10"
+        },
+        {
+            status: "blockchain & web3",
+            color: "text-amber-500",
+            bg: "bg-amber-500/10"
+        },
+        {
+            status: "product management",
+            color: "text-violet-500",
+            bg: "bg-violet-500/10"
+        },
+        {
+            status: "database systems",
+            color: "text-blue-600",
+            bg: "bg-blue-600/10"
+        },
+        {
+            status: "quality assurance",
+            color: "text-lime-600",
+            bg: "bg-lime-600/10"
+        },
+        {
+            status: "open source",
+            color: "text-green-600",
+            bg: "bg-green-600/10"
+        },
+        {
+            status: "tech career & leadership",
+            color: "text-yellow-600",
+            bg: "bg-yellow-600/10"
         }
-    ]
+    ];
     return (
         <>
         {create.map((e, idx)=> {
             if(e.status === status) {
-                return (
+                    return (
                     <div 
                         key={idx}
                         className={`flex items-center justify-center
